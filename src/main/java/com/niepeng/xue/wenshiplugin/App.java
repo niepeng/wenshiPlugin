3897719b1ee3c5b4f826aafe559378984457fc07
niepeng <niepeng>
2017-08-02 22:13:25 +0800

    ad project .

---
+++ src/main/java/com/niepeng/xue/wenshiplugin/App.java
@@ -0,0 +1,19 @@
+package com.niepeng.xue.wenshiplugin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 聂鹏
+ * @version 1.0
+ * @date 17/8/2
+ */
+@SpringBootApplication
+public class App {
+
+  public static void main(String args []) {
+    SpringApplication.run(App.class, args);
+  }
+
+}
---
+++ src/main/java/com/niepeng/xue/wenshiplugin/common/T.java
@@ -0,0 +1,11 @@
+package com.niepeng.xue.wenshiplugin.common;
+
+/**
+ * @author 聂鹏
+ * @version 1.0
+ * @date 17/8/2
+ */
+
+public class T {
+
+}
